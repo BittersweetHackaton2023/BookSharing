93b673b290f1c5db812741e5065d7e17c11e11b1
Ha3To <lgsc0409ld@gmail.com>
2023-06-24 02:04:31 +0900

    도서검색 결과 XML형식으로 전달

--- webproject/book/models/member_models.py
+++ webproject/book/models/member_models.py
@@ -5,4 +5,4 @@ from django.db import models
 
 class Member(models.Model):
     email = models.EmailField(unique=True)
-    mileage = models.IntegerField(default=0)
+    mileage = models.IntegerField(default=100)
