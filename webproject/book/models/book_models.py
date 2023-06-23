e625f57d0f6aff3697590efa6d328c776fa9f470
Ha3To <lgsc0409ld@gmail.com>
2023-06-23 20:41:12 +0900

    book_model.py & book_views.py

--- webproject/book/models/book_models.py
+++ webproject/book/models/book_models.py
@@ -4,3 +4,6 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13)
+
+    def __str__(self):
+        return self.title
