d1a3f045c18fda04d8d78219075e4a2f750d2868
rjswjddn <rjswjddn@gmail.com>
2023-06-23 22:12:21 +0900

    1012

--- webproject/book/models/member_models.py
+++ webproject/book/models/member_models.py
@@ -1,1 +1,8 @@
 from django.db import models
+
+class Member:
+    email = models.EmailField(unique=True)
+    mileage = models.PositiveIntegerField(default = 0)
+
+    def __str__(self):
+        return self.email
