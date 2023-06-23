2bd77a3d2144451e0fd27bd14d2892cb40999d48
rjswjddn <rjswjddn@gmail.com>
2023-06-24 02:14:43 +0900

    0214

--- webproject/book/models/member_models.py
+++ webproject/book/models/member_models.py
@@ -5,4 +5,10 @@ from django.db import models
 
 class Member(models.Model):
     email = models.EmailField(unique=True)
-    mileage = models.IntegerField(default=0)
+    mileage = models.PositiveIntegerField(default=100)
+
+
+class Order(models.Model):
+    isbn = models.CharField()
+    email = models.EmailField(unique=True)
+    mileage = models.PositiveIntegerField(default=0)
