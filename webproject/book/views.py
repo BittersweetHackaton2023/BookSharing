d96987cf442f9c2e921445e2adc8c0119e53cce9
HYUHO LEE <leehh41563636@gmail.com>
2023-06-24 02:42:50 +0900

    views.py 결합

--- webproject/book/views.py
+++ webproject/book/views.py
@@ -1,7 +1,102 @@
 from django.shortcuts import render
+from models.book_models import Book
+from django.db.models import Q
 
-# Create your views here.
+
+## 도서 검색하기(제목, 저자, ISBN 중 1개이상의 키워드를 이용하여)
+def search_books(request):
+    query = request.GET.get('query')  # 검색어를 GET 파라미터로 받아옴
+
+    # 책 검색
+    books = None
+
+    if query:
+        query_parts = query.split()
+
+        # 검색 쿼리 동적 구성
+        q_objects = Q()
+
+        for part in query_parts:
+            q_objects |= Q(title__icontains=part) | Q(author__icontains=part) | Q(isbn__icontains=part)
+
+        books = Book.objects.filter(q_objects)
+
+    context = {
+        'query': query,
+        'books': books,
+    }
+
+    return render(request, 'search_results.html', context)
+
+
+
+from models.member_models import *
+from book.form import *
+from django.http import HttpResponse
+
+## email이 있는지 체크
+def checkemail(email):
+    try:
+        member = Member.objects.get(email=email)
+        return member
+    except Member.DoesNotExist:
+        return None
+
+
+## email의 마일리지 확인
+def mymileage(request):
+    if request.method == 'POST':
+        form = Emailform(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            member = checkemail(email)
+            if member:
+                mileage = member.mileage
+                return render(request, '.html', {'mileage': mileage})
+            else:
+                member = registeremail(email)
+                mileage = member.mileage
+                return render(request, '.html', {'mileage': mileage})
+    else:
+        form = Emailform()
+    
+    context = {'form': form}
+    return render(request, '.html', context)
+
+
+## 이메일 DB에 등록
+def registeremail(email):
+    member = Member(email = email)
+    member.save()
+    return member
+
+
+
+def usemileage(request):
+    if request.method == 'POST':
+        form = Mileageform(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            mileage = form.cleaned_data['mileage']
+            member = checkemail(email)
+            if member:
+                member.mileage -= mileage
+                return render(request, '.html', {'mileage': mileage})
+            else:
+                member = registeremail(email)
+                mileage = member.mileage
+                return render(request, '.html', {'mileage': mileage})
+    else:
+        form = Emailform()
+    
+    context = {'form': form}
+    return render(request, '.html', context)
+
+
+
+
 def index(request):
     return render(request, 'book/index.html')
+
 def register(request):
     return render(request, 'book/register.html')
