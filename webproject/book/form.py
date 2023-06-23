9bcbe278fffcafc1977f4231f5bbfc431ba9108e
Ha3To <lgsc0409ld@gmail.com>
2023-06-24 02:49:55 +0900

    사용자가 도서를 등록하면, 해당 도서 정보를 리스트로 저장

--- webproject/book/form.py
+++ webproject/book/form.py
@@ -1,5 +1,7 @@
 from models.member_models import *
 from django.forms import ModelForm
+from models.book_models import Book
+
 
 class Emailform(ModelForm):
     class Meta:
@@ -10,3 +12,8 @@ class Mileageform(ModelForm):
     class Meta:
         model = Member
         field = ['email', 'mileage']
+
+class BookForm(ModelForm):
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'isbn', 'email']
--- webproject/book/models/book_models.py
+++ webproject/book/models/book_models.py
@@ -4,6 +4,7 @@ class Book(models.Model):
     title = models.CharField(max_length=100)
     author = models.CharField(max_length=100)
     isbn = models.CharField(max_length=13)
+    email = models.EmailField()
 
     def __str__(self):
         return self.title
