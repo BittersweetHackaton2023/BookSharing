c809eb934124eeb6840a124dfd938988228cf86c
rjswjddn <rjswjddn@gmail.com>
2023-06-23 22:11:18 +0900

    1011

--- webproject/book/views/member_views.py
+++ webproject/book/views/member_views.py
@@ -1,1 +1,8 @@
 from django.shortcuts import render
+from models.member_models import *
+from django.contrib.auth.decorators import login_required
+
+
+def mymileage(request):
+    member = request.user.member
+    email = models.EmailField(unique=True)
